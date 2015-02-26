d0d9fdaae027e028d07d79bba98416d6b6f84bb0
wayne2k <wayne2k@gmail.com>
2015-02-26 18:25:27 +0200

    Creating a Custom Inspector

---
+++ Assets/Editor/ListTesterInspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ListTester))]
+public class ListTesterInspector : Editor {
+
+	public override void OnInspectorGUI () {
+		serializedObject.Update();
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("integers"), true);
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("vectors"), true);
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("colorPoints"), true);
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("objects"), true);
+		serializedObject.ApplyModifiedProperties();
+	}
+}
